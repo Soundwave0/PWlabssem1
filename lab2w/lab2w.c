97a05ae21ae388819e89a18ec234116c7d83f685
Soundwave0 <104643493+Soundwave0@users.noreply.github.com>
2023-11-02 12:29:15 +0100

    lab2

---
+++ lab2w/lab2w.c
@@ -0,0 +1,118 @@
+// lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#define PI 3.1415926535897932384626433833279601184
+
+int main()
+{
+	long float pi = 3.1415926535897932384626433833279601184;
+	long double pi2 = 3.1415926535897932384626433833279601184;
+	//printf("%Lf\n", pi);
+	//printf("%.40lf",pi);
+	//double dob = 0.1 + 0.1 + 0.1;
+	//float flo = 0.1f + 0.1f + 0.1f;
+	//printf("%d", dob);
+	//printf("%Lf", flo);
+	//PI = 3;
+	//printf("%.100d", M_PI);
+
+	/*double rad = 4.5;
+
+	long double circum = M_PI * rad * 2;
+
+	long double area = M_PI * rad * rad;
+
+	printf("%lf : is the circumference \n", circum);
+	printf("%lf :  is the area of the circle", area);*/
+
+	//int remainder = 25 % 7;
+	//printf("%i", remainder);
+	/*
+	double c = 4.1;
+	double b = 3.2;
+	double sol;
+
+	sol = sqrt(pow(c, 2) - pow(b, 2));
+	printf("%f", sol);
+	*/
+	/*int a = 7;
+	int b = 2;
+	printf("%.2f", ((float)a / (float)b));
+	*/
+	/*
+	char mychar = 'a';
+	mychar = mychar + 4;
+	printf("%c", mychar);
+	//It goes into th char takes the ascii value then it adds four to the value of the ascii char
+	//then converts into char
+	*/
+	/*
+	int input;
+	printf("Enter your lucky number: \n");
+	scanf("%d", &input);
+	printf("the number is %d. \n", input);
+
+	*/
+	/*
+	int first;
+	int second;
+	printf("please enter a number : ");
+
+	scanf("%i", &first);
+	
+	printf("please print another number: ");
+
+	scanf("%i", &second);
+
+	printf("\nthe sum of %i and %i is %i", first, second, first + second);
+	*/
+	/*
+	float a;
+	float b;
+	printf("please enter first: ");
+	scanf("%f", &a);
+	printf("please enter second: ");
+	scanf("%f", &b);
+	float quotient = a / b;
+
+	printf("the quotient of %f and %f is %f", a, b, quotient);
+	*/
+	/*
+	char name[16];
+	char lastname[16];
+	printf("enter your first name:  \n");
+	scanf("%s ", name);
+	printf("enter your last name : ");
+	scanf("%s", lastname);
+	printf("your name is %s \n your last name is %s . \n", name, lastname);
+	*/
+	/*
+	char adj1[10];
+	char noun[10];
+	char verb[10];
+	
+
+	printf("please enter an adjective: ");
+	scanf("%s", adj1);
+	printf("\nplease enter a noun : ");
+	scanf("%s", noun);
+	printf("\nplease enter a verb: ");
+	scanf("%s", verb);
+	printf("\n");
+	printf("programming is %s and %s is the lord of c programming, he can write a compiler while %s", adj1, noun, verb);
+
+
+	*/
+
+
+	
+
+
+
+
+
+	return 0;
+}
